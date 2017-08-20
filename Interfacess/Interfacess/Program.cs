9a17ed66702e94ae19e1330264681711da17bb6d
bhargavak <bhargavak37@gmail.com>
2017-08-20 10:56:55 +0530

    Interfaces concept in c#
    Interfaces concept in c#

---
+++ Interfacess/Interfacess/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    using System;
+
+    interface ITitled
+    {
+        string Title { get; }
+    }
+
+    interface IExample
+    {
+        void SomeMethod();
+    }
+
+    class Person : ITitled, IExample
+    {
+   
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        public virtual string Title
+        {
+            get
+            {
+                return "";
+            }
+        }
+        public override string ToString()
+        {
+        if (string.IsNullOrWhiteSpace(Title))
+            return Name;
+        else
+            return $"{Title} {Name}";
+        }
+    void IExample.SomeMethod()
+    {
+    }
+    void M()
+    {
+        this.SomeMethod();
+        IExample example = this;
+        example.SomeMethod();
+    }
+    }
+
+sealed class President : Person,IExample
+{
+    public President(string name) : base(name)
+    {
+
+    }
+    public override string Title
+    {
+        get
+        {
+            return "President";
+        }
+    }
+}
+abstract class ChangeRequest : ITitled
+{
+    protected ChangeRequest(string title, Person implementer)
+    {
+        Title = title;
+        Implementer = implementer;
+    }
+        public string Title { get; set; }
+    public Person Implementer { get; set; }
+
+    public void SubmitRequest()
+    {
+        ValidateRequest();
+    }
+
+
+
+
+
+
+
+    }
+}
+
